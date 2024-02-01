63d13a93521637ec49a786ceb75bf44ee07a3d12
hnam-truong <90917843+hnam-truong@users.noreply.github.com>
2024-02-01 17:07:11 +0700

    Update Header.tsx

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -7,12 +7,12 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className="header">
-{/*       <img className="header-logo" src="./assets/Logo.png" alt="@logo" /> */}
+      <img className="header-logo" src="" alt="@logo" />
       <div className="header-user">
         <div className="header-organization">
           <div className="wrapper-logo-content">
-{/*             <img className="organization-logo" src="./assets/organization.png" alt="@organization" /> */}
-            <span>uniGate</span>
+            <img className="organization-logo" src="" alt="@organization" />
+{/*             <span>uniGate</span> */}
           </div>
         </div>
         <AvatarUser />
