315e8c26965341ccca09fc996aa38f89e1a88348
hnam-truong <truonghahaonam@gmail.com>
2024-04-01 14:54:36 +0700

    10th

--- src/interfaces/activity-log.inteface.ts
+++
@@ -1,9 +0,0 @@
-export interface IActivityLog {
-  ID: string;
-  TemplateID: string;
-  Category: string;
-  DateTime: string;
-  Sender: string;
-  Receiver: string;
-  Cc: string;
-}
--- src/utils/EnhanceText.tsx
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const boldText = (text: string) => <b>{text}</b>;
