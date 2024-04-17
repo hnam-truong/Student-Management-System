32d9dc98aad3f4eb2e947e8b11aeda623717dcb8
hnam-truong <90917843+hnam-truong@users.noreply.github.com>
2024-04-17 08:46:02 +0700

    Update ExcelTemplates.ts

--- src/constants/ExcelTemplates.ts
+++ src/constants/ExcelTemplates.ts
@@ -1,9 +1,9 @@
 enum ExcelTemplates {
-  Student = "../../../../public/assets/files/StudentTemplate.xlsx",
-  ReservedStudent = "../../../../public/assets/files/ReservedStudentTemplate.xlsx",
-  StudentScore = "../../../../public/assets/files/StudentScoreTemplate.xlsx",
-  StudentClass = "../../../../public/assets/files/StudentClassTemplate.xlsx",
-  User = "../../../../public/assets/files/UserTemplate.xlsx",
+  Student = "/assets/files/StudentTemplate.xlsx",
+  ReservedStudent = "/assets/files/ReservedStudentTemplate.xlsx",
+  StudentScore = "/assets/files/StudentScoreTemplate.xlsx",
+  StudentClass = "/assets/files/StudentClassTemplate.xlsx",
+  User = "/assets/files/UserTemplate.xlsx",
 }
 
 export default ExcelTemplates;
